1ec77c26c2f5b2cb84f33b676c4160717444145e
qloog <quanlongwang@gmail.com>
2018-09-25 23:30:44 +0800

    add error code

--- pkg/errno/code.go
+++ pkg/errno/code.go
@@ -15,4 +15,7 @@ var (
 	ErrUserNotFound      = &Errno{Code: 20102, Message: "The user was not found."}
 	ErrTokenInvalid      = &Errno{Code: 20103, Message: "The token was invalid."}
 	ErrPasswordIncorrect = &Errno{Code: 20104, Message: "The password was incorrect."}
+
+	// course errors
+	ErrCourseNotFound      = &Errno{Code: 20201, Message: "The course was not found."}
 )
