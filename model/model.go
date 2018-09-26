339a2f3e71f50786601dc87d0807126874a28cae
qloog <quanlongwang@gmail.com>
2018-09-26 22:07:42 +0800

    modify json

--- model/model.go
+++ model/model.go
@@ -5,8 +5,8 @@ import (
 )
 
 type BaseModel struct {
-	Id        uint64     `gorm:"primary_key;AUTO_INCREMENT;column:id" json:"-"`
-	CreatedAt time.Time  `gorm:"column:created_at" json:"-"`
-	UpdatedAt time.Time  `gorm:"column:updated_at" json:"-"`
+	Id        uint64     `gorm:"primary_key;AUTO_INCREMENT;column:id" json:"id"`
+	CreatedAt time.Time  `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time  `gorm:"column:updated_at" json:"updated_at"`
 	DeletedAt *time.Time `gorm:"column:deleted_at" sql:"index" json:"-"`
 }
