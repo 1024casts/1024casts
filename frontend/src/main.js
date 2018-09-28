024d3043a01bf59a7abb9ff55cb775a55cfdb51c
qloog <quanlongwang@gmail.com>
2018-09-28 19:57:48 +0800

    modify and add

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+import Cookies from 'js-cookie'
+import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/styles/index.css' // global css
 import App from './App'
 import router from './router'
+import store from './store'
 
-Vue.use(ElementUI, {
-  size: 'small',
-  zIndex: 3000
+import i18n from './lang' // Internationalization
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
 })
 
 Vue.config.productionTip = false
@@ -18,6 +22,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
+  i18n,
   components: { App },
   template: '<App/>'
 })
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Layout from '@/views/layout/Layout'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      component: () => import('@/views/login/index'),
+      hidden: true
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Layout,
+      redirect: '/dashboard',
+      children: [
+        {
+          path: 'dashboard',
+          component: () => import('@/views/dashboard/index'),
+          name: 'Dashboard',
+          meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
+        }
+      ]
     }
   ]
 })
