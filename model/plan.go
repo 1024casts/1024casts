04403a8e5d708da6caa045d8f9ac54a012887b72
qloog <quanlongwang@gmail.com>
2018-10-17 16:24:55 +0800

    add field

--- model/plan.go
+++ model/plan.go
@@ -6,20 +6,22 @@ import (
 )
 
 type PlanModel struct {
-	ID          uint64    `gorm:"column:id" json:"id"`
-	Alias       string    `gorm:"column:alias" json:"alias"`
-	CreatedAt   time.Time `gorm:"column:created_at" json:"created_at"`
-	DeletedAt   time.Time `gorm:"column:deleted_at" json:"deleted_at"`
-	Description string    `gorm:"column:description" json:"description"`
-	Name        string    `gorm:"column:name" json:"name"`
-	Price       float64   `gorm:"column:price" json:"price"`
-	PromoEnd    time.Time `gorm:"column:promo_end" json:"promo_end"`
-	PromoPrice  float64   `gorm:"column:promo_price" json:"promo_price"`
-	PromoStart  time.Time `gorm:"column:promo_start" json:"promo_start"`
-	Status      int       `gorm:"column:status" json:"status"`
-	UpdatedAt   time.Time `gorm:"column:updated_at" json:"updated_at"`
-	UserID      int       `gorm:"column:user_id" json:"user_id"`
-	ValidDays   int       `gorm:"column:valid_days" json:"valid_days"`
+	ID              uint64    `gorm:"column:id" json:"id"`
+	Alias           string    `gorm:"column:alias" json:"alias"`
+	Description     string    `gorm:"column:description" json:"description"`
+	Name            string    `gorm:"column:name" json:"name"`
+	Price           float64   `gorm:"column:price" json:"price"`
+	PromoEnd        time.Time `gorm:"column:promo_end" json:"promo_end"`
+	PromoPrice      float64   `gorm:"column:promo_price" json:"promo_price"`
+	PromoStart      time.Time `gorm:"column:promo_start" json:"promo_start"`
+	Status          int       `gorm:"column:status" json:"status"`
+	UserID          int       `gorm:"column:user_id" json:"user_id"`
+	ValidDays       int       `gorm:"column:valid_days" json:"valid_days"`
+	CreatedAt       time.Time `gorm:"column:created_at" json:"created_at"`
+	DeletedAt       time.Time `gorm:"column:deleted_at" json:"deleted_at"`
+	UpdatedAt       time.Time `gorm:"column:updated_at" json:"updated_at"`
+	FormatCreatedAt string    `json:"format_created_at"`
+	FormatUpdatedAt string    `json:"format_updated_at"`
 }
 
 // TableName sets the insert table name for this struct type
