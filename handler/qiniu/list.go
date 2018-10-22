cd5956d731bde0b59a6f967b4358420a210c79e0
qloog <quanlongwang@gmail.com>
2018-10-23 00:01:57 +0800

    add qiniu list

---
+++ handler/qiniu/list.go
@@ -0,0 +1,59 @@
+package qiniu
+
+import (
+	. "1024casts/backend/handler"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/qiniu/api.v7/auth/qbox"
+	"github.com/qiniu/api.v7/storage"
+	"github.com/spf13/viper"
+)
+
+func List(c *gin.Context) {
+
+	accessKey := viper.GetString("qiniu.AccessKey")
+	secretKey := viper.GetString("qiniu.SecretKey")
+	bucket := viper.GetString("qiniu.Bucket")
+
+	mac := qbox.NewMac(accessKey, secretKey)
+
+	cfg := storage.Config{
+		// 是否使用https域名进行资源管理
+		UseHTTPS: false,
+	}
+	// 指定空间所在的区域，如果不指定将自动探测
+	// 如果没有特殊需求，默认不需要指定
+	//cfg.Zone=&storage.ZoneHuabei
+	bucketManager := storage.NewBucketManager(mac, &cfg)
+
+	infos := make([]storage.ListItem, 0)
+
+	limit := 10
+	prefix := c.Query("prefix")
+	delimiter := ""
+	//初始列举marker为空
+	marker := c.Query("next_marker")
+
+	entries, _, nextMarker, hashNext, err := bucketManager.ListFiles(bucket, prefix, delimiter, marker, limit)
+	if err != nil {
+		fmt.Println("list error,", err)
+	}
+	//print entries
+	for _, entry := range entries {
+		//fmt.Println(entry.Key)
+		infos = append(infos, entry)
+
+	}
+	if hashNext {
+		fmt.Println("nextMarker", nextMarker)
+		marker = nextMarker
+	}
+
+	SendResponse(c, nil, ListResponse{
+		TotalCount: uint64(len(infos)),
+		List:       infos,
+		NextMaker:  marker,
+	})
+
+}
---
+++ handler/qiniu/qiniu.go
@@ -0,0 +1,38 @@
+package qiniu
+
+import (
+	"1024casts/backend/model"
+
+	"github.com/qiniu/api.v7/storage"
+)
+
+type CreateRequest struct {
+	Name        string `json:"name"`
+	Type        string `json:"type"`
+	Description string `json:"description"`
+	Slug        string `json:"slug"`
+	CoverImage  string `json:"cover_image"`
+	UserId      int    `json:"user_id"`
+	IsPublish   int    `json:"is_publish"`
+}
+
+type CreateResponse struct {
+	Id uint64 `json:"id"`
+}
+
+type ListRequest struct {
+	Name  string `json:"name"`
+	Page  int    `json:"page"`
+	Limit int    `json:"limit"`
+}
+
+type ListResponse struct {
+	TotalCount uint64             `json:"totalCount"`
+	List       []storage.ListItem `json:"list"`
+	NextMaker  string             `json:"next_maker"`
+}
+
+type SwaggerListResponse struct {
+	TotalCount uint64               `json:"totalCount"`
+	List       []model.CommentModel `json:"list"`
+}
