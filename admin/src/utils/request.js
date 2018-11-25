3533b28ff1d4f48ecaadd4bcf260a4736db9f085
qloog <quanlongwang@gmail.com>
2018-11-25 23:49:08 +0800

    modify res.code

--- admin/src/utils/request.js
+++ admin/src/utils/request.js
@@ -42,7 +42,7 @@ service.interceptors.response.use(
         duration: 5 * 1000
       })
       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.code === 20103) {
         // 请自行在引入 MessageBox
         // import { Message, MessageBox } from 'element-ui'
         MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
