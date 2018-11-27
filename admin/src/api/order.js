b6eb7f704e45480a7d96fad521191125499f8e44
qloog <quanlongwang@gmail.com>
2018-11-27 16:44:56 +0800

    move to 1024casts-admin

--- admin/src/api/order.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchList (query) {
-  return request({
-    url: '/v1/orders',
-    method: 'get',
-    params: query
-  })
-}
--- admin/src/api/video.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchVideoList (courseId) {
-  return request({
-    url: '/v1/videos/' + courseId,
-    method: 'get'
-  })
-}
