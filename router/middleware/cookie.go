55e924e17cbb822afe7aa916df406b7ac00cd1e3
qloog <quanlongwang@gmail.com>
2019-03-16 23:33:12 +0800

    optimize code

--- router/middleware/cookie.go
+++ router/middleware/cookie.go
@@ -5,27 +5,29 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
+	"github.com/spf13/viper"
 )
 
 func CookieMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		c.SetCookie("Cookie", "123456", 3600, "/", "localhost:8888", false, true)
+		log.Infof("test cookie: %+v", viper.GetString("cookie.name"))
 
-		if cookie, err := c.Request.Cookie("Cookie"); err == nil {
-			value := cookie.Value
+		cookie, err := c.Request.Cookie(viper.GetString("cookie.name"))
+		if err != nil {
+			log.Warnf("[middleware] get cookie err: %+v", err)
+			c.Redirect(http.StatusMovedPermanently, "/login")
+		}
+		value := cookie.Value
 
-			c.Next()
+		c.Next()
 
-			if len(value) == 0 {
-				cookie.Value = ""
-				log.Infof("cookie...... %+v", cookie)
-				c.Redirect(http.StatusMovedPermanently, "/login")
-			} else {
-				c.SetCookie("Cookie", cookie.Value, 3600, "/", "localhost:8888", false, true)
-			}
+		if len(value) == 0 {
+			cookie.Value = ""
+			log.Infof("cookie...... %+v", cookie)
+			c.Redirect(http.StatusMovedPermanently, "/login")
 		} else {
-			log.Warnf("Get cookie err")
+			c.SetCookie(viper.GetString("cookie.name"), cookie.Value, viper.GetInt("cookie.max_age"), "/", "localhost:8888", false, true)
 		}
 	}
 }
