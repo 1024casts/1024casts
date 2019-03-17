2978525bd63b70546dcde3d711ed4ae3710ff814
qloog <quanlongwang@gmail.com>
2019-03-17 22:45:43 +0800

    new add

---
+++ handler/web/user/profile.go
@@ -0,0 +1,15 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/1024casts/1024casts/util"
+	"github.com/gin-gonic/gin"
+)
+
+func Profile(c *gin.Context) {
+	c.HTML(http.StatusOK, "user/profile", gin.H{
+		"title":   "个人资料",
+		"user_id": util.GetUserId(c),
+	})
+}
