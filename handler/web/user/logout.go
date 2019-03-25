b502bbab66d730fc04101e0869a14477ffbd5ba5
qloog <quanlongwang@gmail.com>
2019-03-25 18:11:42 +0800

    fix

--- handler/web/user/logout.go
+++ handler/web/user/logout.go
@@ -1,14 +1,10 @@
 package user
 
 import (
-	"github.com/1024casts/1024casts/model"
-	"github.com/1024casts/1024casts/pkg/auth"
-	"github.com/1024casts/1024casts/pkg/errno"
-	"github.com/1024casts/1024casts/pkg/token"
-	"github.com/1024casts/1024casts/service"
+	"net/http"
 
-	"github.com/1024casts/1024casts/pkg/app"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 // @Summary Login generates the authentication token
@@ -18,33 +14,9 @@ import (
 // @Success 200 {string} json "{"code":0,"message":"OK","data":{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MjgwMTY5MjIsImlkIjowLCJuYmYiOjE1MjgwMTY5MjIsInVzZXJuYW1lIjoiYWRtaW4ifQ.LjxrK9DuAwAzUD8-9v43NzWBN7HXsSLfebw92DKd1JQ"}}"
 // @Router /login [post]
 func Logout(c *gin.Context) {
-	// Binding the data with the user struct.
-	var u LoginCredentials
-	if err := c.Bind(&u); err != nil {
-		app.Response(c, errno.ErrBind, nil)
-		return
-	}
 
-	srv := service.NewUserService()
-	// Get the user information by the login username.
-	d, err := srv.GetUserByEmail(u.Email)
-	if err != nil {
-		app.Response(c, errno.ErrUserNotFound, nil)
-		return
-	}
+	c.SetCookie(viper.GetString("cookie.name"), "", 0, "/", "localhost:8888", false, true)
 
-	// Compare the login password with the user password.
-	if err := auth.Compare(d.Password, u.Password); err != nil {
-		app.Response(c, errno.ErrPasswordIncorrect, nil)
-		return
-	}
-
-	// Sign the json web token.
-	t, err := token.Sign(c, token.Context{ID: d.Id, Username: d.Username}, "")
-	if err != nil {
-		app.Response(c, errno.ErrToken, nil)
-		return
-	}
-
-	app.Response(c, nil, model.Token{Token: t})
+	c.Redirect(http.StatusMovedPermanently, "/")
+	c.Abort()
 }
