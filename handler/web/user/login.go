aadd9c65ab5fe4b2ad53f9efc9c7c4ddb2395650
qloog <quanlongwang@gmail.com>
2019-03-25 23:30:23 +0800

    rename func name

--- handler/web/user/login.go
+++ handler/web/user/login.go
@@ -27,7 +27,7 @@ func GetLogin(c *gin.Context) {
 // @Param password body string true "Password"
 // @Success 200 {string} json "{"code":0,"message":"OK","data":{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MjgwMTY5MjIsImlkIjowLCJuYmYiOjE1MjgwMTY5MjIsInVzZXJuYW1lIjoiYWRtaW4ifQ.LjxrK9DuAwAzUD8-9v43NzWBN7HXsSLfebw92DKd1JQ"}}"
 // @Router /login [post]
-func Login(c *gin.Context) {
+func DoLogin(c *gin.Context) {
 	// Binding the data with the user struct.
 	var u LoginCredentials
 	if err := c.Bind(&u); err != nil {
