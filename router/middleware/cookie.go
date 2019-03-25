af479a441970f7b7ee7d13b75b6b052bc2feab6a
qloog <quanlongwang@gmail.com>
2019-03-25 22:54:14 +0800

    fix

--- router/middleware/cookie.go
+++ router/middleware/cookie.go
@@ -10,21 +10,15 @@ import (
 
 func CookieMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		cookie, err := c.Request.Cookie(viper.GetString("cookie.name"))
-		if err != nil {
-			log.Warnf("[middleware] get cookie err: %+v", err)
+		cookie, _ := c.Cookie(viper.GetString("cookie.name"))
+		log.Infof("current cookie: %s", cookie)
+		if len(cookie) == 0 {
 			c.Redirect(http.StatusMovedPermanently, "/login")
+			c.Abort()
+		} else {
+			c.SetCookie(viper.GetString("cookie.name"), cookie, viper.GetInt("cookie.max_age"), "/", "localhost:8888", false, true)
 		}
-		value := cookie.Value
 
 		c.Next()
-
-		if len(value) == 0 {
-			cookie.Value = ""
-			log.Infof("current cookie %+v", cookie)
-			c.Redirect(http.StatusMovedPermanently, "/login")
-		} else {
-			c.SetCookie(viper.GetString("cookie.name"), cookie.Value, viper.GetInt("cookie.max_age"), "/", "localhost:8888", false, true)
-		}
 	}
 }
