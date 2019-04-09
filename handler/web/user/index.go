06a274fb117f5e1af437e25ec88cd940592422e3
qloog <quanlongwang@gmail.com>
2019-04-10 00:22:42 +0800

    fix error data

--- handler/web/user/index.go
+++ handler/web/user/index.go
@@ -4,22 +4,24 @@ import (
 	"net/http"
 
 	"github.com/1024casts/1024casts/service"
-	"github.com/1024casts/1024casts/util"
 	"github.com/gin-gonic/gin"
 )
 
 func Index(c *gin.Context) {
-	userId := util.GetUserId(c)
+	userName := c.Param("username")
 	srv := service.NewUserService()
-	user, err := srv.GetUserById(userId)
+	user, err := srv.GetUserByUsername(userName)
 	if err != nil {
-		c.HTML(http.StatusNotFound, "error/404", gin.H{})
+		c.HTML(http.StatusNotFound, "error/404", gin.H{
+			"title": "404错误",
+			"ctx":   c,
+		})
 		return
 	}
 
 	c.HTML(http.StatusOK, "user/index", gin.H{
 		"title":   "个人中心",
-		"user_id": userId,
+		"user_id": user.Id,
 		"user":    user,
 		"ctx":     c,
 		"add": func(a int, b int) int {
