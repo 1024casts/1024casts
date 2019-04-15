60e586c0f0788bdf89bab9476b370a22c4358f2c
qloog <quanlongwang@gmail.com>
2019-04-15 22:24:14 +0800

    modify password

--- handler/web/user/password.go
+++ handler/web/user/password.go
@@ -3,9 +3,14 @@ package user
 import (
 	"net/http"
 
+	"github.com/1024casts/1024casts/pkg/app"
+	"github.com/1024casts/1024casts/pkg/auth"
+	"github.com/1024casts/1024casts/pkg/errno"
 	"github.com/1024casts/1024casts/service"
 	"github.com/1024casts/1024casts/util"
 	"github.com/gin-gonic/gin"
+	"github.com/lexkong/log"
+	"github.com/spf13/viper"
 )
 
 func Password(c *gin.Context) {
@@ -27,4 +32,65 @@ func Password(c *gin.Context) {
 			return a + b
 		},
 	})
+}
+
+type PasswordRequest struct {
+	CurrentPassword      string `json:"current_password" form:"current_password"`
+	Password             string `json:"password" form:"password"`
+	PasswordConfirmation string `json:"password_confirmation" form:"password_confirmation"`
+}
+
+// 更新基本资料
+func DoPassword(c *gin.Context) {
+	var req PasswordRequest
+	if err := c.Bind(&req); err != nil {
+		app.Response(c, errno.ErrBind, nil)
+		return
+	}
+
+	userId := util.GetUserId(c)
+	srv := service.NewUserService()
+
+	user, err := srv.GetUserById(userId)
+	if err != nil {
+		app.Response(c, errno.ErrUserNotFound, nil)
+		return
+	}
+
+	// check 旧密码是否和数据库密码一致
+	if err := auth.Compare(user.Password, req.CurrentPassword); err != nil {
+		log.Warn("[user] current password is neq password")
+		app.Response(c, errno.ErrPasswordIncorrect, nil)
+		return
+	}
+
+	// check 新密码是否和确认密码一致
+	if req.Password != req.PasswordConfirmation {
+		log.Warnf("[user] password neq user password_confirmation err: %v", err)
+		app.Response(c, errno.ErrPasswordIncorrect, nil)
+		return
+	}
+
+	hashedPassword, err := auth.Encrypt(req.Password)
+	if err != nil {
+		log.Warnf("[user] Encrypt user password err: %v", err)
+		app.Response(c, errno.ErrEncrypt, nil)
+		return
+	}
+
+	userMap := map[string]interface{}{
+		"password": hashedPassword,
+	}
+	err = srv.UpdateUser(userMap, userId)
+	if err != nil {
+		log.Warnf("[user] update user password err: %v", err)
+		app.Response(c, errno.ErrDatabase, nil)
+		return
+	}
+
+	// 重新登录
+	c.SetCookie(viper.GetString("cookie.name"), "", 0, "/", "localhost:8888", false, true)
+
+	app.Response(c, errno.OK, nil)
+	return
 }
