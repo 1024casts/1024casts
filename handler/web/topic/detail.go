9b9051c8d43f6519ed738c4bf40edf1a90a875ec
qloog <quanlongwang@gmail.com>
2019-04-30 17:02:14 +0800

    add topic data

--- handler/web/topic/detail.go
+++ handler/web/topic/detail.go
@@ -2,6 +2,9 @@ package topic
 
 import (
 	"net/http"
+	"strconv"
+
+	"github.com/lexkong/log"
 
 	"github.com/1024casts/1024casts/service"
 	"github.com/1024casts/1024casts/util"
@@ -10,14 +13,27 @@ import (
 
 func Detail(c *gin.Context) {
 	userId := util.GetUserId(c)
-	srv := service.NewUserService()
+	userSrv := service.NewUserService()
 
-	user, _ := srv.GetUserById(userId)
+	user, _ := userSrv.GetUserById(userId)
+
+	topicId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Warnf("[topic] get topic id err: %+v", err)
+		topicId = 0
+	}
+
+	srv := service.NewTopicService()
+	topic, err := srv.GetTopicById(topicId)
+	if err != nil {
+		log.Warnf("[topic] get topic detail err: %+v", err)
+	}
 
 	c.HTML(http.StatusOK, "topic/detail", gin.H{
 		"title":   "话题详情",
 		"user_id": userId,
 		"user":    user,
+		"topic":   topic,
 		"ctx":     c,
 	})
 }
