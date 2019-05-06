388185e1bd77924716ace39fae375e49e300ac75
qloog <quanlongwang@gmail.com>
2019-05-06 23:29:40 +0800

    add pages

--- handler/web/course/index.go
+++ handler/web/course/index.go
@@ -1,7 +1,12 @@
 package course
 
 import (
+	"html/template"
 	"net/http"
+	"strconv"
+
+	"github.com/1024casts/1024casts/pkg/constvar"
+	"github.com/1024casts/1024casts/pkg/pagination"
 
 	"github.com/lexkong/log"
 
@@ -15,20 +20,28 @@ func Index(c *gin.Context) {
 	srv := service.NewUserService()
 	user, _ := srv.GetUserById(userId)
 
+	page, err := strconv.Atoi(c.Query("page"))
+	if err != nil {
+		log.Error("get page error", err)
+	}
+	limit := constvar.DefaultLimit
+	offset := (page - 1) * limit
+
 	courseSrv := service.NewCourseService()
 	courseMap := make(map[string]interface{})
 	courseMap["is_publish"] = 1
-	courses, count, err := courseSrv.GetCourseList(courseMap, 0, 20)
+	courses, count, err := courseSrv.GetCourseList(courseMap, offset, 12)
 	if err != nil {
 		log.Warnf("[course] get course list err: %v", err)
 	}
 
+	pagination := pagination.NewPagination(c.Request, count, limit)
 	c.HTML(http.StatusOK, "course/index", gin.H{
 		"title":   "视频课程",
 		"user_id": userId,
 		"user":    user,
 		"ctx":     c,
-		"count":   count,
 		"courses": courses,
+		"pages":   template.HTML(pagination.Pages()),
 	})
 }
