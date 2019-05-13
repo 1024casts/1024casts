e07f864349a10b87bf4e1784c66e759994df43c3
qloog <quanlongwang@gmail.com>
2019-05-13 21:23:33 +0800

    use BaseModel

--- model/topic.go
+++ model/topic.go
@@ -7,11 +7,9 @@ import (
 )
 
 type TopicModel struct {
+	BaseModel
 	Body            string    `gorm:"column:body" json:"body"`
 	CategoryID      int       `gorm:"column:category_id" json:"category_id"`
-	CreatedAt       time.Time `gorm:"column:created_at" json:"created_at"`
-	DeletedAt       time.Time `gorm:"column:deleted_at" json:"deleted_at"`
-	Id              uint64    `gorm:"column:id" json:"id"`
 	IsBlocked       string    `gorm:"column:is_blocked" json:"is_blocked"`
 	IsExcellent     string    `gorm:"column:is_excellent" json:"is_excellent"`
 	LastReplyTimeAt time.Time `gorm:"column:last_reply_time_at" json:"last_reply_time_at"`
@@ -20,7 +18,6 @@ type TopicModel struct {
 	ReplyCount      int       `gorm:"column:reply_count" json:"reply_count"`
 	Source          string    `gorm:"column:source" json:"source"`
 	Title           string    `gorm:"column:title" json:"title"`
-	UpdatedAt       time.Time `gorm:"column:updated_at" json:"updated_at"`
 	UserID          uint64    `gorm:"column:user_id" json:"user_id"`
 	ViewCount       int       `gorm:"column:view_count" json:"view_count"`
 	VoteCount       int       `gorm:"column:vote_count" json:"vote_count"`
