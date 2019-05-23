56741f6ca91321e016454c53fe34ae0377b7ca14
qloog <quanlongwang@gmail.com>
2019-05-23 22:41:21 +0800

    new add

--- handler/web/comment/like.go
+++ handler/web/comment/like.go
@@ -1,13 +1,37 @@
 package comment
 
 import (
+	"strconv"
+
+	"github.com/1024casts/1024casts/service"
+	"github.com/lexkong/log"
+
 	"github.com/1024casts/1024casts/pkg/app"
+	"github.com/1024casts/1024casts/pkg/errno"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Like(c *gin.Context) {
-	//slug := c.Param("slug")
+	commentId := c.Param("comment_id")
+	cmtId, err := strconv.Atoi(commentId)
+	if err != nil {
+		app.Response(c, errno.ErrParam, nil)
+		return
+	}
+
+	cmtSrv := service.NewCommentService()
+	comment, err := cmtSrv.GetCommentById(cmtId)
+	if err != nil {
+		log.Warnf("[comment] get comment err: %v", err)
+		app.Response(c, err, nil)
+	}
+	if comment.Id == 0 {
+		app.Response(c, errno.ErrDataIsNotExist, nil)
+		return
+	}
+
+	cmtSrv.IncrLikeCount(cmtId)
 
 	app.Response(c, nil, nil)
 }
