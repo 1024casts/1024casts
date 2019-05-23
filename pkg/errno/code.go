56e409972f050e2e78c2956799fcabc6df07f845
qloog <quanlongwang@gmail.com>
2019-05-23 22:41:08 +0800

    add new err

--- pkg/errno/code.go
+++ pkg/errno/code.go
@@ -5,6 +5,8 @@ var (
 	OK                  = &Errno{Code: 0, Message: "OK"}
 	InternalServerError = &Errno{Code: 10001, Message: "Internal server error"}
 	ErrBind             = &Errno{Code: 10002, Message: "Error occurred while binding the request body to the struct."}
+	ErrParam            = &Errno{Code: 10003, Message: "Param error"}
+	ErrDataIsNotExist   = &Errno{Code: 10004, Message: "该数据不存在"}
 
 	ErrValidation    = &Errno{Code: 20001, Message: "Validation failed."}
 	ErrDatabase      = &Errno{Code: 20002, Message: "Database error."}
