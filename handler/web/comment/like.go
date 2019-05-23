281b3b995a9481dc3199d0b805353c30ba6d5cc1
qloog <quanlongwang@gmail.com>
2019-05-23 08:51:32 +0800

    new add

---
+++ handler/web/comment/like.go
@@ -0,0 +1,13 @@
+package comment
+
+import (
+	"github.com/1024casts/1024casts/pkg/app"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Like(c *gin.Context) {
+	//slug := c.Param("slug")
+
+	app.Response(c, nil, nil)
+}
