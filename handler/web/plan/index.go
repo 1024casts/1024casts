c8b143246ebf06f354f6d8870b2abc0f9cc323f0
qloog <quanlongwang@gmail.com>
2019-05-30 23:23:56 +0800

    get plans data

--- handler/web/plan/index.go
+++ handler/web/plan/index.go
@@ -3,6 +3,8 @@ package plan
 import (
 	"net/http"
 
+	"github.com/lexkong/log"
+
 	"github.com/1024casts/1024casts/service"
 	"github.com/1024casts/1024casts/util"
 	"github.com/gin-gonic/gin"
@@ -11,13 +13,21 @@ import (
 func Index(c *gin.Context) {
 	userId := util.GetUserId(c)
 	srv := service.NewUserService()
-
 	user, _ := srv.GetUserById(userId)
+
+	planMap := make(map[string]interface{})
+	planMap["status"] = 1
+	planSrv := service.NewPlanService()
+	plans, _, err := planSrv.GetPlanList(planMap, 0, 10)
+	if err != nil {
+		log.Warnf("[plan] get plan list err: %v", err)
+	}
 
 	c.HTML(http.StatusOK, "plan/index", gin.H{
 		"title":   "VIP订阅",
 		"user_id": userId,
 		"user":    user,
 		"ctx":     c,
+		"plans":   plans,
 	})
 }
