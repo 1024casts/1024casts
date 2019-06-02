c784953706b1e9c62adcb0e9752edf1d123073b9
qloog <quanlongwang@gmail.com>
2019-06-02 21:43:16 +0800

    update ReplyModel

--- model/reply.go
+++ model/reply.go
@@ -3,21 +3,17 @@ package model
 import (
 	"html/template"
 	"sync"
-	"time"
 )
 
 type ReplyModel struct {
-	Id         uint64    `gorm:"column:id" json:"id"`
-	Body       string    `gorm:"column:body" json:"body"`
-	UserId     uint64    `gorm:"column:user_id" json:"user_id"`
-	IsBlocked  string    `gorm:"column:is_blocked" json:"is_blocked"`
-	OriginBody string    `gorm:"column:origin_body" json:"origin_body"`
-	Source     string    `gorm:"column:source" json:"source"`
-	TopicID    int       `gorm:"column:topic_id" json:"topic_id"`
-	CreatedAt  time.Time `gorm:"column:created_at" json:"created_at"`
-	DeletedAt  time.Time `gorm:"column:deleted_at" json:"deleted_at"`
-	UpdatedAt  time.Time `gorm:"column:updated_at" json:"updated_at"`
-	VoteCount  int       `gorm:"column:vote_count" json:"vote_count"`
+	BaseModel
+	Body       string `gorm:"column:body" json:"body"`
+	UserId     uint64 `gorm:"column:user_id" json:"user_id"`
+	IsBlocked  string `gorm:"column:is_blocked" json:"is_blocked"`
+	OriginBody string `gorm:"column:origin_body" json:"origin_body"`
+	Source     string `gorm:"column:source" json:"source"`
+	TopicID    int    `gorm:"column:topic_id" json:"topic_id"`
+	VoteCount  int    `gorm:"column:vote_count" json:"vote_count"`
 }
 
 // TableName sets the insert table name for this struct type
