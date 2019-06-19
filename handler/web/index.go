28bae8d08f72c8c6a9ab596c4c38ab4c021e4d83
qloog <quanlongwang@gmail.com>
2019-06-19 18:10:28 +0800

    get courses

--- handler/web/index.go
+++ handler/web/index.go
@@ -6,6 +6,7 @@ import (
 	"github.com/1024casts/1024casts/service"
 	"github.com/1024casts/1024casts/util"
 	"github.com/gin-gonic/gin"
+	log "qiniupkg.com/x/log.v7"
 )
 
 func Index(c *gin.Context) {
@@ -14,11 +15,20 @@ func Index(c *gin.Context) {
 
 	user, _ := srv.GetUserById(userId)
 
+	courseSrv := service.NewCourseService()
+	courseMap := make(map[string]interface{})
+	courseMap["is_publish"] = 1
+	courses, _, err := courseSrv.GetCourseList(courseMap, 0, 6)
+	if err != nil {
+		log.Warnf("[course] get course list err: %v", err)
+	}
+
 	c.HTML(http.StatusOK, "index", gin.H{
 		"title":   "首页",
 		"user_id": userId,
 		"user":    user,
 		"ctx":     c,
+		"courses": courses,
 		"add": func(a int, b int) int {
 			return a + b
 		},
