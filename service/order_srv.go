35280c72183278f4f66eb099847a9b6d08d70508
qloog <quanlongwang@gmail.com>
2019-07-02 23:17:46 +0800

    update func: createOrder

--- service/order_srv.go
+++ service/order_srv.go
@@ -4,7 +4,14 @@ import (
 	"sync"
 	"time"
 
+	"github.com/1024casts/1024casts/pkg/constvar"
+
+	"github.com/qingwg/payjs"
+	"github.com/qingwg/payjs/native"
+	"github.com/spf13/viper"
+
 	"github.com/1024casts/1024casts/util"
+	"github.com/lexkong/log"
 
 	"github.com/1024casts/1024casts/model"
 	"github.com/1024casts/1024casts/repository"
@@ -20,13 +27,93 @@ func NewOrderService() *OrderService {
 	}
 }
 
-func (srv *OrderService) CreateOrder(orderModel model.OrderModel) (uint64, error) {
-	orderId, err := srv.orderRepo.CreateOrder(orderModel)
-	if err != nil {
-		return 0, err
+func (srv *OrderService) CreateOrder(userId uint64, plan *model.PlanModel) (id uint64, qrCodeUrl string, err error) {
+
+	db := model.DB.Self
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+	if tx.Error != nil {
+		return 0, "", err
 	}
 
-	return orderId, nil
+	orderId := util.GenOrderNo()
+	orderInput := model.OrderModel{
+		Id:          orderId,
+		PayMethod:   constvar.PayMethodWeiXin,
+		OrderAmount: plan.Price,
+		PayAmount:   plan.Price,
+		UserId:      userId,
+		Status:      constvar.OrderStatusPending,
+	}
+	// step1: create main order
+	if err := tx.Create(&orderInput).Error; err != nil {
+		tx.Rollback()
+		log.Warnf("[order] create user err: %v", err)
+		return 0, "", err
+	}
+
+	// step2: create pay order
+	payResp, err := srv.CreatePayOrder(orderId, plan)
+	if err != nil {
+		tx.Rollback()
+		log.Warnf("[order] create user pay order err: %v", err)
+		return 0, "", err
+	}
+
+	// step3: write to order items
+	orderItemModel := model.OrderItemModel{
+		OrderID:  orderId,
+		UserID:   userId,
+		ItemID:   plan.ID,
+		Name:     plan.Name,
+		Price:    plan.Price,
+		Quantity: 1,
+		Amount:   plan.Price * 1,
+	}
+	if err := tx.Create(&orderItemModel).Error; err != nil {
+		tx.Rollback()
+		log.Warnf("[order] create order item err: %v", err)
+		return 0, "", err
+	}
+
+	// step4: 更新第三方平台订单号到order表里
+	if err := tx.Model(&model.OrderModel{}).Where("id = ?", orderId).
+		UpdateColumn("trade_id = ?", payResp.PayJSOrderID).Error; err != nil {
+		tx.Rollback()
+		log.Warnf("[order] update order err: %v", err)
+		return 0, "", err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		log.Warnf("[order] commit order fail, err: %+v", err)
+		return 0, "", err
+	}
+
+	return orderId, payResp.Qrcode, nil
+}
+
+// 调用第三方服务创建支付订单
+func (srv *OrderService) CreatePayOrder(orderId uint64, plan *model.PlanModel) (native.CreateResponse, error) {
+	payJsConfig := &payjs.Config{
+		Key:       viper.GetString("pay_js.secret_key"),
+		MchID:     viper.GetString("pay_js.mch_id"),
+		NotifyUrl: "",
+	}
+	pay := payjs.New(payJsConfig)
+	payNative := pay.GetNative()
+	payAmount := plan.Price * 100
+	resp, err := payNative.Create(int64(payAmount), plan.Name, string(orderId), "", "")
+	if err != nil {
+		log.Warnf("[plans] create pay order err: %v", err)
+		return native.CreateResponse{}, err
+	}
+	log.Infof("[plans] pay create resp: %+v", resp)
+	return resp, nil
 }
 
 func (srv *OrderService) GetOrderById(id int) (*model.OrderModel, error) {
@@ -170,20 +257,10 @@ func (srv *OrderService) trans(orderModel *model.OrderModel) *model.OrderInfo {
 		CanceledAt:  time.Time{},
 		QrcodeId:    0,
 		TradeId:     "",
-		Status:      util.GetPayStatusText(orderModel.Status),
+		Status:      orderModel.Status,
 		UserId:      orderModel.UserId,
 		CreatedAt:   util.TimeToString(orderModel.CreatedAt),
 		UpdatedAt:   util.TimeToString(orderModel.UpdatedAt),
 		OrderItems:  orderModel.Items,
 	}
-}
-
-func (srv *OrderService) UpdateComment(commentMap map[string]interface{}, id int) error {
-	err := srv.orderRepo.UpdateComment(commentMap, id)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
