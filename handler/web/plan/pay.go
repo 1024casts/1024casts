9d63a623221fc2cc7e2f722cdeadaaf6ae1ef96b
qloog <quanlongwang@gmail.com>
2019-07-02 23:14:25 +0800

    move biz logic to order service

--- handler/web/plan/pay.go
+++ handler/web/plan/pay.go
@@ -3,13 +3,6 @@ package plan
 import (
 	"net/http"
 
-	"github.com/1024casts/1024casts/pkg/constvar"
-
-	"github.com/1024casts/1024casts/model"
-
-	"github.com/qingwg/payjs"
-	"github.com/spf13/viper"
-
 	"github.com/lexkong/log"
 
 	"github.com/1024casts/1024casts/service"
@@ -28,41 +21,10 @@ func Pay(c *gin.Context) {
 		log.Warnf("[plan] get plan list err: %v", err)
 	}
 
-	// step1: 创建订单
 	orderSrv := service.NewOrderService()
-	orderModel := model.OrderModel{}
-	orderModel.Id = util.GenOrderNo()
-	orderModel.PayMethod = constvar.PayMethodWeiXin
-	orderModel.OrderAmount = plan.Price
-	orderModel.PayAmount = plan.Price
-	orderModel.UserId = userId
-	orderModel.Status = constvar.OrderStatusPending
-	orderId, err := orderSrv.CreateOrder(orderModel)
+	orderId, qrCodeUrl, err := orderSrv.CreateOrder(userId, plan)
 	if err != nil {
 		log.Warnf("[plans] create order err: %v", err)
-	}
-
-	var qrCodeUrl string
-	if orderId > 0 {
-		// step2: 创建支付订单
-		payJsConfig := &payjs.Config{
-			Key:       viper.GetString("pay_js.secret_key"),
-			MchID:     viper.GetString("pay_js.mch_id"),
-			NotifyUrl: "",
-		}
-		pay := payjs.New(payJsConfig)
-		payNative := pay.GetNative()
-		payAmount := plan.Price * 100
-		resp, err := payNative.Create(int64(payAmount), plan.Name, string(orderId), "", "")
-		if err != nil {
-			log.Warnf("[plans] create pay order err: %v", err)
-		}
-		log.Infof("[plans] pay create resp: %+v", resp)
-		qrCodeUrl = resp.Qrcode
-
-		// step3: write to order items
-
-		// step4: 更新相关数据到order
 	}
 
 	c.HTML(http.StatusOK, "plan/pay", gin.H{
