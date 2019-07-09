ab1797d9d667b65095d44dcced5bd833a7181d97
qloog <quanlongwang@gmail.com>
2019-07-09 22:18:35 +0800

    update

--- handler/web/github/callback.go
+++ handler/web/github/callback.go
@@ -2,8 +2,14 @@ package github
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/1024casts/1024casts/model"
+	"github.com/1024casts/1024casts/service"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/lexkong/log"
 )
 
 // Redirect to correct oAuth URL
@@ -14,16 +20,56 @@ func CallbackHandler(c *gin.Context) {
 	state := c.Query("state")
 
 	// Handle callback and check for errors
-	user, token, err := gocial.Handle(state, code)
+	githubUser, _, err := gocial.Handle(state, code)
 	if err != nil {
-		c.Writer.Write([]byte("Error: " + err.Error()))
+		log.Warnf("[github] callback err: %v", err)
+		c.Redirect(http.StatusFound, "/login/oauth/github")
 		return
 	}
 
 	// Print in terminal user information
-	fmt.Printf("%#v", token)
-	fmt.Printf("%#v", user)
+	//fmt.Printf("%#v", token)
+	//fmt.Printf("%#v", user)
 
-	// If no errors, show provider name
-	c.Writer.Write([]byte("Hi, " + user.FullName))
+	fmt.Printf("user_id: %s", githubUser.ID)
+
+	// 如果用户存在，则更新最后登录时间和ip
+	userSrv := service.NewUserService()
+	user, err := userSrv.GetUserByGithubId(githubUser.ID)
+
+	var userId uint64
+	// if not exist, 创建新用户
+	if err == gorm.ErrRecordNotFound {
+		u := model.UserModel{
+			Username:      githubUser.Username,
+			Avatar:        githubUser.Avatar,
+			Password:      "",
+			Email:         githubUser.Email,
+			GithubId:      githubUser.ID,
+			LastLoginTime: time.Now(),
+			LastLoginIp:   c.ClientIP(),
+		}
+		userId, err = userSrv.CreateUser(u)
+		if err != nil {
+			log.Warnf("[github] create user err: %v", err)
+			c.Redirect(http.StatusFound, "/login")
+			return
+		}
+	} else {
+		// get github info, err is nil
+		if err == nil {
+			err := userSrv.UpdateLastLoginInfo(user.Id, c.ClientIP())
+			if err != nil {
+				log.Warnf("[github] update user login info err: %v", err)
+				c.Redirect(http.StatusFound, "/login")
+				return
+			}
+		}
+	}
+
+	// 执行登录操作
+	userSrv.SetLoginCookie(c, userId)
+
+	c.Redirect(http.StatusFound, "/")
+	return
 }
