2ad0fc2b84b7f011c4351095850e88c6c1685b37
qloog <quanlongwang@gmail.com>
2019-07-10 22:59:13 +0800

    update

--- router/middleware/cookie.go
+++ router/middleware/cookie.go
@@ -3,20 +3,33 @@ package middleware
 import (
 	"net/http"
 
+	"github.com/1024casts/1024casts/util"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
 	"github.com/spf13/viper"
 )
 
+// see: http://researchlab.github.io/2016/03/29/gin-setcookie/
 func CookieMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		cookie, _ := c.Cookie(viper.GetString("cookie.name"))
+		cookie, err := c.Cookie(viper.GetString("cookie.name"))
 		log.Infof("current cookie: %s", cookie)
+		if err != nil {
+			log.Warnf("[middleware] err: %v", err)
+			c.Redirect(http.StatusMovedPermanently, "/login")
+			c.Abort()
+			return
+		}
+
 		if len(cookie) == 0 {
+			log.Warnf("[middleware] current cookie len is zero")
 			c.Redirect(http.StatusMovedPermanently, "/login")
 			c.Abort()
 		} else {
-			c.SetCookie(viper.GetString("cookie.name"), cookie, viper.GetInt("cookie.max_age"), "/", "localhost:8888", false, true)
+			userId := util.GetUserId(c)
+			log.Infof("current user_id: %d from cookie", userId)
+			util.SetLoginCookie(c, userId)
 		}
 
 		c.Next()
