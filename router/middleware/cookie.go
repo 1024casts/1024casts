8c71d4b020c0518c3a5688e7cd80e8b6cd0372f4
qloog <quanlongwang@gmail.com>
2019-07-12 00:05:27 +0800

    use gorilla session

--- router/middleware/cookie.go
+++ router/middleware/cookie.go
@@ -3,33 +3,26 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/1024casts/1024casts/util"
+	"github.com/1024casts/1024casts/handler"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
-	"github.com/spf13/viper"
 )
 
 // see: http://researchlab.github.io/2016/03/29/gin-setcookie/
 func CookieMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		cookie, err := c.Cookie(viper.GetString("cookie.name"))
-		log.Infof("current cookie: %s", cookie)
-		if err != nil {
-			log.Warnf("[middleware] err: %v", err)
+		session := handler.GetCookieSession(c)
+		log.Infof("current session: %s", session)
+		userId, ok := session.Values["user_id"]
+		log.Infof("current user_id: %d from cookie", userId)
+		if ok {
+			handler.SetLoginCookie(c, userId.(uint64))
+		} else {
+			log.Warnf("[middleware] current user_id is not ok")
 			c.Redirect(http.StatusMovedPermanently, "/login")
 			c.Abort()
 			return
-		}
-
-		if len(cookie) == 0 {
-			log.Warnf("[middleware] current cookie len is zero")
-			c.Redirect(http.StatusMovedPermanently, "/login")
-			c.Abort()
-		} else {
-			userId := util.GetUserId(c)
-			log.Infof("current user_id: %d from cookie", userId)
-			util.SetLoginCookie(c, userId)
 		}
 
 		c.Next()
