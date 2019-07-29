a5283f5d26d0405f854587626a17f8fce4e4cc4d
qloog <quanlongwang@gmail.com>
2019-07-29 22:48:36 +0800

    add topic list

--- handler/web/user/index.go
+++ handler/web/user/index.go
@@ -1,7 +1,14 @@
 package user
 
 import (
+	"html/template"
 	"net/http"
+	"strconv"
+
+	"github.com/1024casts/1024casts/pkg/pagination"
+
+	"github.com/1024casts/1024casts/pkg/constvar"
+	"github.com/lexkong/log"
 
 	"github.com/1024casts/1024casts/service"
 	"github.com/gin-gonic/gin"
@@ -19,11 +26,29 @@ func Index(c *gin.Context) {
 		return
 	}
 
+	// get topic list
+	topicSrv := service.NewTopicService()
+	page, err := strconv.Atoi(c.Query("page"))
+	if err != nil {
+		log.Error("get page error", err)
+	}
+	limit := constvar.DefaultLimit
+	offset := (page - 1) * limit
+	topicMap := make(map[string]interface{})
+	topicMap["user_id"] = user.Id
+	topics, count, err := topicSrv.GetTopicList(topicMap, offset, limit)
+	if err != nil {
+		log.Warnf("[topic] get topic list err: %v", err)
+	}
+	pagination := pagination.NewPagination(c.Request, count, limit)
+
 	c.HTML(http.StatusOK, "user/index", gin.H{
 		"title":   "个人中心",
 		"user_id": user.Id,
 		"user":    user,
 		"ctx":     c,
+		"topics":  topics,
+		"pages":   template.HTML(pagination.Pages()),
 		"add": func(a int, b int) int {
 			return a + b
 		},
