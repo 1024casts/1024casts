f57c66dbbfd9286e902e28997ef96541393363a8
qloog <quanlongwang@gmail.com>
2019-08-01 20:39:37 +0800

    add log

--- handler/web/user/logout.go
+++ handler/web/user/logout.go
@@ -11,13 +11,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// @Summary Login generates the authentication token
-// @Produce  json
-// @Param username body string true "Username"
-// @Param password body string true "Password"
-// @Success 200 {string} json "{"code":0,"message":"OK","data":{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MjgwMTY5MjIsImlkIjowLCJuYmYiOjE1MjgwMTY5MjIsInVzZXJuYW1lIjoiYWRtaW4ifQ.LjxrK9DuAwAzUD8-9v43NzWBN7HXsSLfebw92DKd1JQ"}}"
-// @Router /login [post]
 func Logout(c *gin.Context) {
+
+	log.Infof("[user] entry logout...")
 
 	// 删除cookie信息
 	session := GetCookieSession(c)
