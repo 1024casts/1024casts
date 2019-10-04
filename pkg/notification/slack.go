86028ade563f316c07be62691eda6c85c18ae349
qloog <quanlongwang@gmail.com>
2019-10-04 23:09:35 +0800

    add new func

--- pkg/notification/slack.go
+++ pkg/notification/slack.go
@@ -16,13 +16,39 @@ type SlackRequestBody struct {
 	Channel string `json:"channel"`
 }
 
-// SendSlackNotification will post to an 'Incoming Webook' url setup in Slack Apps. It accepts
+// SendNewRegisterUserNotification will post to an 'Incoming Webook' url setup in Slack Apps. It accepts
 // some text and the slack channel is saved within Slack.
-func SendSlackNotification(msg string) error {
+func SendNewRegisterUserNotification(msg string) error {
 
 	webhookUrl := viper.GetString("slack.webhook_url")
 
 	slackBody, _ := json.Marshal(SlackRequestBody{Text: msg, Channel: "new-reg-user"})
+	req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(slackBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	if buf.String() != "ok" {
+		return errors.New("Non-ok response returned from Slack")
+	}
+	return nil
+}
+
+func SendNewCommentSlackNotification(msg string) error {
+
+	webhookUrl := viper.GetString("slack.webhook_url")
+
+	slackBody, _ := json.Marshal(SlackRequestBody{Text: msg, Channel: "new-comment"})
 	req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(slackBody))
 	if err != nil {
 		return err
